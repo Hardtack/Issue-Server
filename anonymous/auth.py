6b4ca9356e6d1334c8288661112cfae26684d79b
hardtack <6566gun@gmail.com>
2013-06-02 03:42:59 +0900

    Fix auth

--- anonymous/auth.py
+++ anonymous/auth.py
@@ -23,7 +23,7 @@ def get_login_user():
 def login_required(fn):
     @wraps(fn)
     def decorator(*args, **kwargs):
-        if getattr(g, 'user') is None:
+        if getattr(g, 'user', None) is None:
             abort(401)
         return fn(*args, **kwargs)
     return decorator
